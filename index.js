699fea2b008a14f7a5329afd73217cd6a7cb27bf
Piero Maddaleni <pieromaddaleni03@gmail.com>
2021-09-02 14:26:22 +0000

    Added wait before restarting so files can update properly.

--- index.js
+++ index.js
@@ -46,6 +46,9 @@ app.get("/update", async (req, res) => {
     }
     console.log(`stdout: ${stdout}`);
   })
+  new Promise((resolve) => {
+    setTimeout(resolve, 5000);
+  });
   process.exit();
 })
 
