dc85ebf07699a20d9b7efb6e85fc0387ffed72c1
Piero Maddaleni <pieromaddaleni03@gmail.com>
2021-09-02 14:13:21 +0000

    Final test?

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const db = new Database();
 const fetch = require('node-fetch');
 const sizeof = require('object-sizeof')
 const { exec } = require("child_process");
-console.log("THIS MEANS THE UPDATE SCRIPT ACTUALLY WORKS LMAO version sixxxxxx")
+console.log("THIS MEANS THE UPDATE SCRIPT ACTUALLY WORKS LMAO version final?")
 
 const genFn = () => {  
   return require('crypto')
